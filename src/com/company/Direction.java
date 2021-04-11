d58aa1cd353a59f823058d610386873f8bb8ebdc
Mia Schultink <mia@schultink.com>
2021-04-11 12:56:39 +0300

    directions

--- src/com/company/Direction.java
+++ src/com/company/Direction.java
@@ -3,9 +3,46 @@ package com.company;
 public class Direction {
     private String name;
     private Direction opposite;
+    private Room north;
+    private Room east;
+    private Room west;
+    private Room south;
+
 
     public Direction(String name){
         this.name = name;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
     }
 
     public String getName() {
